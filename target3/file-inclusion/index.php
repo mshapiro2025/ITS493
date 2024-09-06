1d1d9026be79acf95a278f28dd5287ca7558d6c6
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2024-09-06 10:39:01 -0400

    Update index.php

--- target3/file-inclusion/index.php
+++ target3/file-inclusion/index.php
@@ -1,1 +1,17 @@
-this is index
+<a href="index.php?page=page1.html"><button>page1</button></a><br/>
+<a href="index.php?page=page2.html"><button>page2</button></a><br/>
+<a href="index.php?page=page3.html"><button>page3</button></a><br/>
+<?php
+$page = $_GET['page'];
+echo "<div>";
+if(isset($page))
+{
+ 
+  include("$page");
+}
+else
+{
+  echo "<p>select a page</p>";
+}
+echo "</div>";
+?>
